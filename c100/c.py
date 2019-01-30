bc824480bc67bafd14c95bc0573c53bdd2ef0ee8
ohnari <to0614@tomososo.info>
2019-01-31 02:31:00 +0900

    2019.01.31 home

--- c100/c.py
+++ c100/c.py
@@ -4,30 +4,35 @@ import datetime
 
 PING_BASE = 'chg100'
 
+
 def readCsv():
     data = pd.read_csv('c.csv', index_col='DATE')
-    data['Total'] = data.sum(axis=1)
+    data['S'] = data['S_'] - data['S_F']
+    data['G'] = data['G_'] - data['G_F']
+    data['D'] = data['D_'] - data['D_F']
+    data['T'] = data[['S', 'G', 'D']].sum(axis=1)
     return data
 
 
 def myPlot(df, x):
     fig, ax1 = plt.subplots(figsize=(10, 4))
     ax1.set_xticklabels(x, rotation=90, size="small")
-    plt.bar(df.index, df['Total'], label='Total', align='center')
+    plt.bar(df.index, df['T'], label='Total', align='center')
     plt.legend(loc=4)
 
     ax2 = ax1.twinx()
     ax2.set_xticklabels(x, rotation=90, size="small")
-    plt.plot(df.index, df['SBI'], 'b', label='S')
-    plt.plot(df.index, df['GEM'], 'g', label='G')
-    plt.plot(df.index, df['DOOM'], 'y', label='D')
+    plt.plot(df.index, df['S'], 'b', label='S')
+    plt.plot(df.index, df['G'], 'g', label='G')
+    plt.plot(df.index, df['D'], 'y', label='D')
     plt.legend()
 
+    title = "{:,}".format(df.iloc[-1]['T'])
+    plt.title(title)
     plt.savefig(PING_BASE + str(datetime.date.today()) + '.png')
     plt.show()
 
 
-df = readCsv().iloc[:, [0, 1, 2, 3]]
-#ax = list(readCsv().index)
-ax = readCsv().columns
+df = readCsv().loc[:, ['S', 'G', 'D', 'T']]
+ax = readCsv().index
 myPlot(df, ax)
