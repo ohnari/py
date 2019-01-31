7bf53c7fd27495d02bc0cd5a7c2b74ae66bdd9cb
BuildTools <unconfigured@null.spigotmc.org>
2019-01-31 20:56:51 +0900

    2019.01.31 pub last

--- c100/c.py
+++ c100/c.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
 
-PING_BASE = 'chg100'
+PNG_BASE = 'chg100'
 
 PairLabel = ['S', 'G', 'D']
 PairColor = ['r', 'g', 'b']
@@ -16,13 +16,22 @@ class ReadCsv:
         for l in PairLabel:
             cols.append(l+'x')
             cols.append(l+'y')
-        self.data = pd.read_csv(file, index_col=Index, skiprows=1, names=cols)
-        for i, j in enumerate(PairLabel):
-            self.data[j] = self.data[cols[i*2+1]]-self.data[cols[i*2+2]]
-        self.data['T'] = self.data[PairLabel].sum(axis=1)
+        self.orgData = pd.read_csv(
+            file, index_col=Index, skiprows=1, names=cols)
+        self.data = pd.DataFrame(index=self.orgData.index, columns=PairLabel)
+
+    def getOrgData(self):
+        return self.orgData
 
     def getData(self):
+        for i, j in enumerate(PairLabel):
+            self.data[j] = self.orgData.iloc[:, i*2] - \
+                self.orgData.iloc[:, i*2+1]
+        self.data['T'] = self.data[PairLabel].sum(axis=1)
         return self.data
+
+    def getResuData(self):
+        return self.data.iloc[:, -len(PairLabel)-1:]
 
     def getIndex(self):
         return self.data.index
@@ -39,7 +48,8 @@ def myPlot(df, x):
     fig, ax1 = plt.subplots(figsize=(10, 4))
 
     ax1.set_xticklabels(x, rotation=90, size="small")
-    plt.bar(df.index, df['T'], color='c', label='T', align='center')
+    lv = 'T:' + "{:,}".format(df.iloc[-1]['T'])
+    plt.bar(df.index, df['T'], color='c', label=lv, align='center')
     plt.legend(loc='upper left')
 
     ax2 = ax1.twinx()
@@ -54,11 +64,11 @@ def myPlot(df, x):
     tb = "{:,}".format(df.iloc[-1]['T'])
     title = f"T:{tb} ({ti})"
     plt.title(title)
-    plt.savefig(PING_BASE + str(datetime.date.today()) + '.png')
+    plt.savefig(PNG_BASE + str(datetime.date.today()) + '.png')
     plt.show()
 
 
 data = ReadCsv(CsvFile)
-df = data.getData().iloc[:, -len(PairLabel)-1:]
+df = data.getData()
 ax = data.getIndex()
 myPlot(df, ax)
